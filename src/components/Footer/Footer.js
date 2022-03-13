9b75fb41886b07cd11f690e7dbe520eb5dd07a59
Nekhai <NekhaiYurii@gmail.com>
2022-03-13 09:22:41 +0200

    feat: add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./Footer.scss";
 
 export const Footer = () => {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <p>Copyright © 2022. Privacy Policy.</p>
+    </div>
+  );
 };
