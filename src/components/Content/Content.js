78c143a0d59bee727d2e85af4e4eaa6d2885ea07
Nekhai <NekhaiYurii@gmail.com>
2022-03-13 11:30:03 +0200

    feat: add router

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,6 +1,20 @@
 import React from "react";
 import "./Content.scss";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../../pages/Home/Home";
+import { Cart } from "../../pages/Cart/Cart";
+import { ResetPassword } from "../../pages/Reset-password/reset-password";
+import { Menu } from "../../pages/Menu/Menu";
 
 export const Content = () => {
-  return <div className="content"></div>;
+  return (
+    <div className="content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
+    </div>
+  );
 };
